113aa4e85671bf1726e1fcf9aa1ec4d441b0b52c
asfarley <alexander.s.farley@gmail.com>
2017-10-02 02:12:47 -0600

    Adding files

---
+++ coverage.js
@@ -0,0 +1,19 @@
+function renderCoverage(coverage,elementID,outlineColor,fillColor, elementWidth = 1, elementHeight = 20)
+{
+	var canvas = document.getElementById(elementID);
+	if (canvas.getContext) {
+		var ctx = canvas.getContext('2d');
+		ctx.strokeStyle=outlineColor;
+		ctx.rect(0,0,coverage.length*elementWidth,elementHeight);
+		ctx.stroke();
+		ctx.fillStyle=fillColor;
+		for(var i=0; i<coverage.length;i++)
+		{
+			if(coverage[i] == 1)
+			{
+				ctx.fillRect(i*elementWidth,0,elementWidth,elementHeight);
+				ctx.stroke();
+			}
+		}
+	}
+}
